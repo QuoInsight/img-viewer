bd4984601fb98698cbc25fd32bf86b525c067191
kamleong <kamleong@users.noreply.github.com>
2016-11-25 11:50:32 +0800

    Update img-view.make.bat

--- img-view.make.bat
+++ img-view.make.bat
@@ -1,6 +1,6 @@
 @rem C# Compiler
 "%windir%\Microsoft.NET\Framework\v4.0.30319\csc.exe" ^
  /reference:System.Windows.Forms.dll ^
- /target:winexe  /out:"img-view.exe" ^
+ /win32icon:img-view.ico /target:winexe /out:"img-view.exe" ^
  "img-view.cs"
 pause
